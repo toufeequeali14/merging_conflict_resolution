46b94f4452def9b761b48823054016b77c2514c5
Toufeeque Ali <130752892+toufeequeali14@users.noreply.github.com>
2024-03-11 22:16:07 +0500

    first commit

---
+++ objects.js
@@ -0,0 +1,14 @@
+let userInput = 'name'
+
+let alien = {
+    name:'toufeeque',
+    caste:'khokhar',
+    'work exp years':5
+}
+
+console.log("alien['name']= ",                                             alien['name']);
+console.log("\nalien.name= ",                                              alien.name);
+console.log("alien['work exp years']= ",                                   alien['work exp years'])
+
+// console.log(alien.userInput)            It will give error= undefined    
+console.log("alien[userInput]= ",                                          alien[userInput])     
